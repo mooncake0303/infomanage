368471275f7d4527dd5cb1afdd94daa3966977f1
mooncake0303 <3020967277@qq.com>
2019-10-09 21:52:36 +0800

    用于我的Maven代码测试

---
+++ MyMaven2/src/main/java/com/hunau/text/HelloController.java
@@ -0,0 +1,15 @@
+package com.hunau.text;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by sang on 2018/7/4.
+ */
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "宋悦，学号为201841882205";
+    }
+}
